37678cf6b51669263f4abffb7fb4a29422b8b757
Patryk.Dyminski <patryk-dym@o2.pl>
2021-12-05 13:54:44 +0100

    small refactor

--- app/src/main/java/com/patryk/quickpick/ui/testprocess/TestProcessFragment.kt
+++ app/src/main/java/com/patryk/quickpick/ui/testprocess/TestProcessFragment.kt
@@ -11,7 +11,6 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.patryk.quickpick.ui.orderdetail.OrderDetailFragment
-import com.patryk.quickpick.OrderSummaryActivity
 import com.patryk.quickpick.R
 import com.patryk.quickpick.TestSummaryActivity
 import com.patryk.quickpick.data.*
